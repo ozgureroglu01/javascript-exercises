318bf298903a25b5574ec6f07e6faea816f5e23c
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-07-07 20:03:08 +0300

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let start = 0;
+    let end = 0;
+    let sum = 0;
+    if (typeof (a) !== "number" || typeof (b) !== "number" || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (a < b) {
+        start = a;
+        end = b;
+    }
+    else if (b < a) {
+        start = b;
+        end = a;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
