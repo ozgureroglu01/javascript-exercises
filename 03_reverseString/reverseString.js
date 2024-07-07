e4ad2c7b0a06ecc73b115c3755287f613cfb29d1
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-07-07 18:08:59 +0300

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    const arr = word.split("");
+    arr.reverse();
+    const newWord = arr.join("");
+    return newWord;
 };
 
 // Do not edit below this line
