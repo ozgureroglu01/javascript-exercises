ec419cbc0b373d3f3a9a81380300e25e96a6192c
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-07-07 16:57:24 +0300

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeatTimes) {
+    let newWord = "";
+    if (repeatTimes < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < repeatTimes; i++) {
+        newWord += word;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
