4c31e194d871626d4dfaf671dcd4c5613eae5d85
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-07-07 19:40:42 +0300

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (theArgs.includes(arr[i]) === false) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
