26a15ebe48041815c0bd7a77bf419d8a4e35ab20
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-08-13 00:20:18 +0300

    Competed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce(((accumulator, element) => accumulator + element), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((accumulator, element) => accumulator * element);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+	let arr = [];
+	for (let i = number; i > 0; i--) {
+		arr.push(i);
+	}
+	return arr.reduce(((accumulator, element) => accumulator * element), 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial
 };
