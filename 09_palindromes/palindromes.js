2fa2fc80f16da8c4cfd8dd81a6353750e80f6f1b
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-08-13 13:59:37 +0300

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const letters = [];
+    for (let index = str.length; index >= 0; index--) {
+        letters.push(str[index]);
+    }
+    const word = letters.join("");
+    //This regex (/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g) removes special characters and spaces from str.
+    //regex source: https://rswpthemes.com/how-to-remove-spaces-and-special-characters-from-a-string-in-javascript/#:~:text=In%20JavaScript%2C%20we%20can%20use,remove%20spaces%20and%20special%20characters.
+    const newWord = word.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '').toLocaleLowerCase();
+    const newStr = str.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '').toLocaleLowerCase();
+    return newWord === newStr;
 };
 
 // Do not edit below this line
