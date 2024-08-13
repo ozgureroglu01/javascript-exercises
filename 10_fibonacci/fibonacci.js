c4e11e6324b4f28b90fd2248d1cc1e7d338988f5
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-08-13 16:49:09 +0300

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num == 0) {
+        return 0;
+    }
+    else if (num < 0) {
+        return "OOPS";
+    }
+    let fibonacci = [1, 1];
+    for (let index = 0; index < num - 2; index++) {
+        let nextNumber = fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
+        fibonacci.push(nextNumber);
 
+    }
+
+    return fibonacci[fibonacci.length - 1];
 };
 
 // Do not edit below this line
