51a3ef6925b3a6f9d3df4a2d27bfe24a7ed1be1d
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-08-14 11:20:50 +0300

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    let oldest = people[0];
+    people.forEach((person) => {
+        let age;
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currentYear - person.yearOfBirth;
+        }
+        if (age > (oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth)) {
+            oldest = person;
+        }
+    });
+    return oldest;
 
 };
 
